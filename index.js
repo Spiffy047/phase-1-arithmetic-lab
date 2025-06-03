ddab8219ec550a3985d12aafb7c1ed2616264742
Joe M Kariuki <mwanikijoe1@gmail.com>
2025-06-03 21:25:09 +0300

    operator

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+
+let num1 = 2; 
+let num2 = 31;
+
+
+const multiply = num1 * num2; // This will be 2 * 31 = 62
+const random = Math.floor(Math.random()) + 1;
+let num3 = 9;
+let num4 = 5;
+const mod = num3 % num4;
+
+let max = 20
+let min = 1
+const randomInRange = Math.floor(Math.random() * (max - min + 1)) + min;
